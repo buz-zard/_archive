10a09db8aa0bf2cf730479f3b3d56a0b137c1435
Karolis <pastas.k@gmail.com>
2015-02-11 02:46:09 +0200

    clean up

--- octo-x/src/main/scripts/run.bat
+++
@@ -1,4 +0,0 @@
-ECHO OFF
-TITLE ${artifactId} console
-CLS
-start javaw -jar -Djava.library.path="${release.natives}" "${finalName}"
--- octo-x/src/main/scripts/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar -Djava.library.path="natives/META-INF" "octo-x-1.1.0.jar"
