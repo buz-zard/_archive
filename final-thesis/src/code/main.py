8174542dcf7c6b0019738183d63885bf5f5526e7
Karolis <pastas.k@gmail.com>
2015-05-20 23:44:23 +0300

    data

--- final-thesis/src/code/main.py
+++ final-thesis/src/code/main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*
 from tests import initial_population, ox_params, mr_params
-from tests.final import chrom, cr, mr, ipop, cr_mr, chr_cr, chr_mr
+from tests.final import chrom, cr, mr, ipop, cr_mr, chr_cr, chr_mr, chr_ipop
 
 
 if __name__ == '__main__':
@@ -13,8 +13,11 @@ if __name__ == '__main__':
     mr.run_all()
     cr.run_all()
 
+    chr_ipop.run_all()
+    '''
     chr_cr.run_all()
     chr_mr.run_all()
     cr_mr.run_all()
-    ipop.run_all()
+    #ipop.run_all()
     print '\ndone.\n'
+    '''
--- final-thesis/src/code/tests/final/chr_ipop.py
+++ final-thesis/src/code/tests/final/chr_ipop.py
@@ -7,19 +7,19 @@ from analysis import GenericFig
 
 from .. import condition, TestRunResult, add_solver, green_color, red_color
 
-base_folder = 'final_chr_cr'
+base_folder = 'final_chr_ipop'
 c = condition(100)
 
 
 def run_all():
-    # run_base(10)  # +
-    # run_base(20)  # +
-    # run_base(30)  # +
-    # run_base(40)  # +
-    # run_base(50)  # +
+    run_base(10)  # -
+    run_base(20)  # -
+    run_base(30)  # -
+    run_base(40)  # -
+    run_base(50)  # -
     run_base(60)  # -
     run_base(70)  # -
-    # run_base(80)  # +
+    run_base(80)  # -
     pass
 
 
@@ -27,13 +27,18 @@ def runner():
     return TSPSRunner(g_vln())
 
 
-def base_args(chrom, mr):
+def base_args(chrom, ipop):
     args = GeneticArgs()
     args.set_chr(chrom)
-    args.set_cr(mr)
+    args.set_cr(0.3)
     args.set_mr(0.3)
-    args.set_key("cr_{}".format(mr))
-    args.set_method_initial_population(genetic.INITIAL_POPULATION_RANDOM)
+    if ipop > 0:
+        args.set_method_initial_population(genetic.INITIAL_POPULATION_SEMIACS)
+        args.set_initial_ants(ipop)
+        args.set_key('ipop_', args.get_initial_ants)
+    else:
+        args.set_key('random')
+        args.set_method_initial_population(genetic.INITIAL_POPULATION_RANDOM)
     args.set_method_select(genetic.SELECTION_RANK)
     args.set_method_cross(genetic.CROSSOVER_OX)
     args.set_method_mutate(genetic.MUTATION_SA)
@@ -42,17 +47,16 @@ def base_args(chrom, mr):
 
 def run_base(chrom):
     prefix = " chrom = {}".format(chrom)
-    test_name = u"Rekombinacijos koef."
+    test_name = u"Pradine populiacija"
     runs = 10
-    args1 = base_args(chrom, 0.1)
-    args2 = base_args(chrom, 0.2)
-    args3 = base_args(chrom, 0.3)
-    args4 = base_args(chrom, 0.4)
-    args5 = base_args(chrom, 0.5)
-    args6 = base_args(chrom, 0.6)
-    args7 = base_args(chrom, 0.7)
-    args8 = base_args(chrom, 0.8)
-    args9 = base_args(chrom, 0.9)
+    args1 = base_args(chrom, int(chrom / 8.0 * 1))
+    args2 = base_args(chrom, int(chrom / 8.0 * 2))
+    args3 = base_args(chrom, int(chrom / 8.0 * 3))
+    args4 = base_args(chrom, int(chrom / 8.0 * 4))
+    args5 = base_args(chrom, int(chrom / 8.0 * 5))
+    args6 = base_args(chrom, int(chrom / 8.0 * 6))
+    args7 = base_args(chrom, int(chrom / 8.0 * 7))
+    args8 = base_args(chrom, int(chrom / 8.0 * 8))
 
     r = runner()
     add_solver(r, args1, c, runs)
@@ -63,7 +67,6 @@ def run_base(chrom):
     add_solver(r, args6, c, runs)
     add_solver(r, args7, c, runs)
     add_solver(r, args8, c, runs)
-    add_solver(r, args9, c, runs)
     r.run()
 
     fig = GenericFig(size=120)
@@ -76,9 +79,8 @@ def run_base(chrom):
         result.averaged_x_values(args4.key, red_color(4))
         result.averaged_x_values(args5.key, green_color(4))
         result.averaged_x_values(args6.key, green_color(2))
-        result.averaged_x_values(args7.key, green_color(1))
-        result.averaged_x_values(args8.key, green_color(0))
-        result.averaged_x_values(args9.key, 'g--')
+        result.averaged_x_values(args7.key, green_color(0))
+        result.averaged_x_values(args8.key, 'g--')
 
     result.next_subplot(test_name)
     plot()
