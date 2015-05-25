2fe7ce908d499de5ac508b8f9e3445cf72cb77ed
Karolis <pastas.k@gmail.com>
2015-05-26 00:21:05 +0300

    data

--- final-thesis/src/code/tests/final/cr_ipop.py
+++ final-thesis/src/code/tests/final/cr_ipop.py
@@ -12,9 +12,9 @@ c = condition(100)
 
 
 def run_all():
-    run_base(0.1)  # -
-    run_base(0.2)  # -
-    run_base(0.3)  # -
+    # run_base(0.1)  # +
+    # run_base(0.2)  # +
+    # run_base(0.3)  # +
     run_base(0.4)  # -
     run_base(0.5)  # -
     run_base(0.6)  # -
