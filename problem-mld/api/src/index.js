d6b7ff5bd75689a95702a3f0edd5d52dd7c1aac8
Karolis <pastas.k@gmail.com>
2017-04-30 20:46:31 +0300

    es6 node skeleton

---
+++ problem-mld/api/src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+app.listen(3001, () => {
+  console.log('Api server started on port 3001!')
+});
+
+
+export default app;
