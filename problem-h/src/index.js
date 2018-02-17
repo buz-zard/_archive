75b49bc884153bfffc9efb0e06bd46b41bf45a37
Karolis Šarapnickis <karolis.sarapnickis@tesonet.com>
2018-02-17 17:52:06 +0200

    Add tachyons & babel-polyfill

--- problem-h/src/index.js
+++ problem-h/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.scss';
 
-function App() {
-  return <div>Hello</div>;
-}
+import App from './app';
 
 render(<App />, document.getElementById('app'));
